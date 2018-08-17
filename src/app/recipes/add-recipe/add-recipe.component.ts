cf5e36307209d048b21fcc0cb3835f8bc64e043c
t.kandibolotskaya <t.kandibolotskaya@bet-makers.com>
2018-08-17 19:09:50 +0300

    fixerd

--- src/app/recipes/add-recipe/add-recipe.component.ts
+++ src/app/recipes/add-recipe/add-recipe.component.ts
@@ -3,7 +3,7 @@ import {ShoppingService} from '../../shopping-list/shopping.service';
 import {Ingredient} from '../../shared/ingredient.model';
 import {Recipe} from '../recipe.model';
 import {RecipesService} from '../recipes.service';
-import { NgForm } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-recipe',
@@ -11,46 +11,51 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./add-recipe.component.css']
 })
 export class AddRecipeComponent implements OnInit {
-  @ViewChild('f') form: NgForm;
-  ingredients: Ingredient[];
-  selectedIngredients: Ingredient[] = [];
+  form = new FormGroup({
+    'title': new FormControl(null, Validators.required),
+    'description': new FormControl(null, Validators.required),
+    'imageUrl': new FormControl(null, Validators.required),
+    'ingredients': new FormArray([], Validators.minLength(1)),
+    'author': new FormGroup({
+      'name': new FormControl(null, Validators.required),
+      'age': new FormControl(null, [Validators.required, Validators.pattern(/^\d+$/)])
+    })
+  });
   constructor(
     private shoppingService: ShoppingService,
     private recipesService: RecipesService
   ) { }
 
   ngOnInit() {
-    this.ingredients = this.shoppingService.getIngredients();
-    this.shoppingService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
-      this.ingredients = ingredients;
-    });
+
   }
 
-  setIngredient(event: Event): void {
-    const select = event.target as HTMLSelectElement;
-    const selectedId = select.value;
-    if (this.selectedIngredients.find(({id}) => id === selectedId)) {
-      this.selectedIngredients.filter(({id}) => id !== selectedId);
-    } else {
-      this.selectedIngredients.push(
-        this.ingredients.find(({id}) => id === selectedId)
-      );
+  submitHandler(): void {
+    const {title, description, imageUrl, ingredients} = this.form.value;
+    const newRecipe = new Recipe(
+      title,
+      description,
+      imageUrl,
+      ingredients
+    );
+    this.recipesService.addRecipe(newRecipe);
+  }
+  
+
+  addIngredient(): void {
+    (<FormArray>this.form.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(null, Validators.required),
+        'amount': new FormControl(null, [Validators.required, Validators.pattern(/^\d$/), this.ingredientAmountValidator])
+      })
+    );
+  }
+
+  ingredientAmountValidator(control: FormControl): {[s: string]: boolean} {
+    if (Number(control.value) < 1) {
+      return {
+        'tooSmallAmount': true
+      };
     }
-  }
-
-  saveRecipe(form: NgForm): void {
-    // const newRecipe = new Recipe(
-    //   this.name,
-    //   this.description,
-    //   this.imageUrl,
-    //   this.selectedIngredients
-    // );
-    // console.log(this.selectedIngredients);
-    // this.recipesService.addRecipe(newRecipe);
-    console.log(form.value);
-  }
-
-  clearForm(): void {
-    this.form.form.reset();
   }
 }
